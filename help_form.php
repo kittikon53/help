729185b244fc7c1466eff17cbff28126b680c36f
Kittikon Thamakittipan <76489651+kittikon53@users.noreply.github.com>
2023-10-26 23:47:35 +0700

    help

---
+++ help_form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Follow Up - ช่วยเหลือ</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+<div class="icon-font-awesome">
+    <a href="index.php">
+        <img class="vector" src="home.png" alt="Home">
+    </a>
+    <div class="header">
+        <h1>Follow Up - ช่วยเหลือ</h1>
+    </div>
+
+    <div class="container main-content">
+        <h2>Follow Up คืออะไร?</h2>
+        <ul><li><p>Follow Up คือระบบจัดการและวางแผนงานที่ถูกออกแบบเพื่อช่วยให้คุณสามารถจัดการและวางแผนงานต่าง ๆ อย่างมีประสิทธิภาพและง่ายต่อการใช้งาน ด้วยโปรแกรม Follow Up</p></li></ul>
+
+        <h2>สร้างแผนงาน</h2>
+        <ul><li><p>คุณสามารถสร้างแผนงานที่คุณต้องการดำเนินการ โดยกำหนดรายละเอียดงาน, วันกำหนดส่งของงานและความสำคัญของแต่ละงาน</p></li></ul>
+
+        <h2>ติดตามงาน</h2>
+        <ul><li><p>Follow Up ช่วยให้คุณสามารถดูสถานะและความคืบหน้าของแผนงานที่คุณสร้าง โดยการแสดงกราฟและแผนภาพที่ชัดเจน</p></li></ul>
+
+        <h2>การแจ้งเตือนและกำหนดระยะเวลา</h2>
+        <ul><li><p>ระบบสามารถส่งการแจ้งเตือนเมื่อถึงกำหนดการแจ้งเตือนหรือการกำหนดระยะเวลาการส่งของงานได้</p></li></ul>
+
+        <h2>การปรับแต่ง</h2>
+        <ul><li><p>Follow Up ช่วยให้คุณสามารถปรับแต่งการตั้งค่าตามความต้องการงานของคุณ เช่นเลือกสี Label ของงานได้</p></li></ul>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
